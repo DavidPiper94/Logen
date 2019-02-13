759b7acf52909ea74b429c03d5f160703eb728cf
David Piper <david.piper@udo.edu>
2019-02-13 18:29:44 +0100

    Shifted generating of localizations to subcommand. Added new subcommand to generate json file.

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from lib import TerminalStyle
 import GeneratorIOS
 import GeneratorIOSEnum
 import GeneratorAndroid
-
-##### common #####
+import GeneratorJSON
 
 def checkSourceFilepath(path):
     if not JsonHelper.isJSONFile(path):
@@ -36,15 +35,37 @@ def openDirectory(directory):
 
 def setupParser():
     parser = argparse.ArgumentParser(description = "Generates localization files for iOS and Android projects.")
-    parser.add_argument("source", help = "Path to source directory")
-    parser.add_argument("destination", help = "Path at which destination directory will be created")
-    group = parser.add_mutually_exclusive_group()
+    subparsers = parser.add_subparsers(title = "subcommands", 
+                                       description = "Choose whether converting from a .json file to both an iOS and an Android localization or converting from a .strings iOS localization file or a .xml Android localization file to a common .json file.")
+
+    mainParser = subparsers.add_parser("fromJSON", description = "Converts JSON files to both iOS and Android localiations.")
+    mainParser.add_argument("source", help = "Path to source directory")
+    mainParser.add_argument("destination", help = "Path at which destination directory will be created")
+    group = mainParser.add_mutually_exclusive_group()
     group.add_argument("-m", "--mock", action='store_true', help = "Präfixes all strings with the content of templates/template_common_mock. This can be used to check if all texts are localized.")
     group.add_argument("-l", "--long", action='store_true', help = "Präfixes all strings with the first 300 chars of 'Lorem ipsum' to test long strings.")
+    mainParser.set_defaults(func=generateCommonLocalization)
+
+    subParser = subparsers.add_parser("toJSON", description = "Converts a given .strings iOS localization file or .xml Android localization file to a common JSON localization file.")
+    subParser.add_argument("source", help = "Path to source file")
+    subParser.add_argument("destination", help = "Path where created json file will be saved")    
+    subParser.set_defaults(func=generateJSON)
+
     return parser
 
-def main(sourceFilenames, destinationDirectory, addMockText, addLongText):
+def generateCommonLocalization(args):
     
+    # get source dir and destination dir from given arguments
+    sourceDirectory = args.source
+    destinationDirectory = args.destination
+    addMockText = args.mock
+    addLongText = args.long
+    
+    # get all json files from source directory
+    sourceFilenames = []
+    for file in glob.glob("{}/*.json".format(sourceDirectory)):
+        sourceFilenames.append(file)
+
     # Construct destination paths.
     path = "{}/localization_gen".format(destinationDirectory)
     iosDestinationPath = "{}/ios".format(path)
@@ -69,21 +90,24 @@ def main(sourceFilenames, destinationDirectory, addMockText, addLongText):
     # Open the output directory.
     openDirectory(destinationDirectory)
 
-if __name__ == "__main__":
+def generateJSON(args):
+
+    sourceFilepath = args.source
+    destinationFilepath = args.destination
     
+    extension = FileHelper.fileExtension(sourceFilepath)
+    lines = FileHelper.readLines(sourceFilepath)
+
+    if extension == ".strings":
+        GeneratorJSON.convertIOSToJSON(lines, destinationFilepath)
+    elif extension == ".xml":
+        print("Converting of android localization currently not supported")
+    else:
+        print(TerminalStyle.TerminalStyle.FAIL + "Wrong file format." + TerminalStyle.TerminalStyle.ENDC)
+        print("Please provide .strings file for existing iOS localization or .xml file for existing Android localization.")
+        sys.exit()
+
+if __name__ == "__main__":
     parser = setupParser()
     args = parser.parse_args()
-
-    # get source dir and destination dir from given arguments
-    sourceDirectory = args.source
-    destinationDirectory = args.destination
-    addMockText = args.mock
-    addLongText = args.long
-    
-    # get all json files from source directory
-    sourceFilenames = []
-    for file in glob.glob("{}/*.json".format(sourceDirectory)):
-        sourceFilenames.append(file)
-    
-    # start main function
-    main(sourceFilenames, destinationDirectory, addMockText, addLongText)
+    args.func(args)
