9cfbd5b9056ff59667d4dc356b9254d5a4cd8a58
David Piper <david.piper@udo.edu>
2019-06-26 21:16:08 +0200

    Added structure for AndroidConverter and tests.

---
+++ localizer/tests/tests_AndroidConverter.py
@@ -0,0 +1,14 @@
+import unittest
+
+from localizer.converter.AndroidConverter import AndroidConverter
+
+class TestAndroidConverter(unittest.TestCase):
+
+    def test_generateIntermediateFromAndroid(self):
+        pass
+
+    def test_generateAndroidLocalization(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
