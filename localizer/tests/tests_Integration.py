f52fe186684c5008d41fd7ac459a72a4a4489b16
David Piper <david.piper@udo.edu>
2019-06-26 21:29:46 +0200

    Added empty tests file for integration tests.

---
+++ localizer/tests/tests_Integration.py
@@ -0,0 +1,16 @@
+import unittest
+
+from localizer.converter.JSONConverter import JSONConverter
+from localizer.converter.AndroidConverter import AndroidConverter
+from localizer.converter.iOSConverter import iOSConverter
+
+class TestIntegration(unittest.TestCase):
+
+    def test_import(self):
+        pass
+
+    def test_export(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
