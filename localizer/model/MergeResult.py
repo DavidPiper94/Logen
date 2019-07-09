51c0db9cb410ddad61a064ac5c3bc75b1da14db2
David Piper <david.piper@udo.edu>
2019-07-09 14:17:53 +0300

    Improved naming of properties.

--- localizer/model/MergeResult.py
+++ localizer/model/MergeResult.py
@@ -1,15 +1,15 @@
 class MergeResult():
     
-    def __init__(self, mergedIntermediateLocalization, listOfMissingEntries):
-        self.mergedIntermediateLocalization = mergedIntermediateLocalization
-        self.listOfMissingEntries = listOfMissingEntries
+    def __init__(self, result, missingEntries):
+        self.result = result
+        self.missingEntries = missingEntries
 
     def __eq__(self, other):
         """Override the default Equals behavior"""
-        return self.mergedIntermediateLocalization == other.mergedIntermediateLocalization and self.listOfMissingEntries == other.listOfMissingEntries
+        return self.result == other.result and self.missingEntries == other.missingEntries
     
     def __str__(self):
-        description = "MergeResult of two IntermediateLoclaizations:\n" + str(self.mergedIntermediateLocalization) + "\nMissing Entries:"
-        for entry in self.listOfMissingEntries:
+        description = "MergeResult of two IntermediateLoclaizations:\n" + str(self.result) + "\nMissing Entries:"
+        for entry in self.missingEntries:
             description += "{}, ".format(entry)
         return description
