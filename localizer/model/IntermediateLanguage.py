2a4bb8cd941b913ecac2302b1fc0a495bb3048b3
David Piper <david.piper@udo.edu>
2019-07-09 14:18:59 +0300

    Improved comparing of intermediate localizations with different order of items.

--- localizer/model/IntermediateLanguage.py
+++ localizer/model/IntermediateLanguage.py
@@ -6,10 +6,10 @@ class IntermediateLanguage:
         
     def __eq__(self, other):
         """Override the default Equals behavior"""
-        return self.languageIdentifier == other.languageIdentifier and self.intermediateEntries == other.intermediateEntries
+        return self.languageIdentifier == other.languageIdentifier and self.intermediateEntries.sort(key = lambda x: x.key) == other.intermediateEntries.sort(key = lambda x: x.key)
 
     def __str__(self):
         description = "IntermediateLanguage:\n   LanguageIdentifier: {}\n".format(self.languageIdentifier)
         for entry in self.intermediateEntries:
-            description += str(entry)
+            description += str(entry) + "\n"
         return description
