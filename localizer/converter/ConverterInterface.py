5bc86d40399bddf1361a3e2de901c073c5b1935f
David Piper <david.piper@udo.edu>
2019-07-20 11:18:13 +0300

    Added more documentation for ConverterInterface

--- localizer/converter/ConverterInterface.py
+++ localizer/converter/ConverterInterface.py
@@ -11,16 +11,64 @@ class ConverterInterface:
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractproperty
-    def fileExtension(self): raise NotImplementedError
+    def fileExtension(self): 
+        """A string which defines the extensions of files that can be processed by the specific subclass of converter.
+
+        Returns
+        -------
+            A string containing the extension of a file that can be processed by the specific subclass of converter.
+
+        Raises
+        ------
+        NotImplementedError
+            If this method is not overriden by a subclass.
+        """
+        raise NotImplementedError
 
     @abc.abstractproperty
-    def identifier(self): raise NotImplementedError
+    def identifier(self): 
+        """A string which identifies a specific subclass of converter to select it in the subcommand 'convert'.
+
+        Returns
+        -------
+            A string containing the identifier of the specific subclass of converter to identify it and make it selectable by the command line.
+
+        Raises
+        ------
+        NotImplementedError
+            If this method is not overriden by a subclass.
+        """
+        raise NotImplementedError
 
     @abc.abstractproperty
-    def importDescription(self): raise NotImplementedError
+    def importDescription(self): 
+        """Contains a description of the import function of a specific converter which will be shown by the 'list' subcommand.
+
+        Returns
+        -------
+            A string describing the import function of a specific subclass of converter. This description will be shown by the 'list' subcommand.
+
+        Raises
+        ------
+        NotImplementedError
+            If this method is not overriden by a subclass.
+        """
+        raise NotImplementedError
 
     @abc.abstractproperty
-    def exportDescription(self): raise NotImplementedError
+    def exportDescription(self): 
+        """Contains a description of the export function of a specific converter which will be shown by the 'list' subcommand.
+
+        Returns
+        -------
+            A string describing the export function of a specific subclass of converter. This description will be shown by the 'list' subcommand.
+
+        Raises
+        ------
+        NotImplementedError
+            If this method is not overriden by a subclass.
+        """
+        raise NotImplementedError
 
     @abc.abstractmethod
     def toIntermediate(self, filepath: str) -> IntermediateLocalization:
@@ -29,11 +77,11 @@ class ConverterInterface:
         Parameters
         ----------
         filepath: str
-            Path to file which content will be converted.
+            Path to file from which the content will be converted to an intermediate localization.
 
         Returns
         -------
-        LocalizationFile:
+        IntermediateLocalization:
             Instance of class IntermediateLocalization containing the converted content of the file.
 
         Raises
@@ -41,11 +89,10 @@ class ConverterInterface:
         NotImplementedError
             If no converting method is available for the content.
         """
-
         raise NotImplementedError
 
     @abc.abstractmethod
-    def fromIntermediate(self, intermediateLocalization: IntermediateLocalization) -> LocalizationFile: 
+    def fromIntermediate(self, intermediateLocalization: IntermediateLocalization) -> [LocalizationFile]: 
         """Converts intermediate localization to specific format.
         
         Parameters
@@ -55,25 +102,36 @@ class ConverterInterface:
 
         Returns
         -------
-        LocalizationFile:
-            Instance of class LocalizationFile encapsulating filepath and content.
+        [LocalizationFile]:
+            A list of LocalizationFiles encapsulating filepath and content.
 
         Raises
         ------
         NotImplementedError
             If no converting method is available for this intermediate localization.
         """
-
         raise NotImplementedError
 
     # There may be cases, where it is useful to merge two intermediate localizations together.
-    # E.g. when using this converter and there is one file de.lproj/File.strings and one en.lproj/File.strings,
-    # Than the end result should be one single intermediate localization with both languages combined.
-    # Another aproach would be to handle this case when importing a folder of multiple *.lproj directorys,
+    # E.g. when using the ios converter and there is one file 'de.lproj/File.strings' and one 'en.lproj/File.strings',
+    # than the end result should be one single intermediate localization with both languages combined.
+    # Another aproach would be to handle this case when importing a folder of multiple '*.lproj' directorys,
     # but this would need a special handling on importing. And how should other setups be handled?
     # Thus it is easier to add this method for merging two intermediate localizations together.
-    # This method returns an instance of MergeResult.
-    def merge(self, first: IntermediateLocalization, second: IntermediateLocalization) -> IntermediateLocalization:
+    def merge(self, first: IntermediateLocalization, second: IntermediateLocalization) -> MergeResult:
+        """
+        
+        Parameters
+        ----------
+        first: IntermediateLocalization
+        
+        second: IntermediateLocalization
+
+        Returns
+        -------
+        MergeResult:
+            
+        """
 
         # Make sure, both are objects of type IntermeditateLocalization.
         if not type(first) is IntermediateLocalization or not type(second) is IntermediateLocalization:
