58fc846da1b072e4d95a3439acd149d6932c540b
David Piper <david.piper@udo.edu>
2019-07-23 12:37:02 +0300

    Created spy for converter and added tests for convert subcommand.

---
+++ localizer/tests/ConverterSpy.py
@@ -0,0 +1,53 @@
+from localizer.converter.ConverterInterface import ConverterInterface as Base
+from localizer.model.IntermediateEntry import IntermediateEntry
+from localizer.model.IntermediateLanguage import IntermediateLanguage
+from localizer.model.IntermediateLocalization import IntermediateLocalization
+from localizer.model.LocalizationFile import LocalizationFile
+
+class ConverterSpy(Base):    
+    
+    #--------------------------------------------------
+    # private properties
+    #--------------------------------------------------
+
+    _fileExtension = ""
+    _identifier = ""
+
+    _didImport = False
+    _didExport = False
+
+    #--------------------------------------------------
+    # Methods to change functionality
+    #--------------------------------------------------
+
+    def changeFileExtensionTo(self, newFileExtension):
+        self._fileExtension = newFileExtension
+
+    def changeIdentifierTo(self, newIdentifier):
+        self._identifier = newIdentifier
+
+    #--------------------------------------------------
+    # Methods to check functionality
+    #--------------------------------------------------
+
+    def didImport(self) -> bool:
+        return self._didImport
+
+    def didExport(self) -> bool:
+        return self._didExport
+
+    #--------------------------------------------------
+    # Base class conformance
+    #--------------------------------------------------
+
+    def fileExtension(self): return self._fileExtension
+
+    def identifier(self): return self._identifier
+
+    def importDescription(self): raise NotImplementedError
+
+    def exportDescription(self): raise NotImplementedError
+
+    def toIntermediate(self, filepath): self._didImport = True
+        
+    def fromIntermediate(self, intermediateLocalization): self._didExport = True
