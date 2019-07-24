c6b83e20ce63ed3295870fd329eba6d49c3f4c8a
David Piper <david.piper@udo.edu>
2019-07-24 18:55:14 +0300

    Added test for iOSEnumConverter. Removed some unneccessary spaces in localizer/templates/template_common_generated_warning.txt which leeded to errors in test case. Added this change in testdata for iOSConverter and AndroidConverter tests.

---
+++ localizer/tests/testdata/FileNameLocalizableKeys.swift
@@ -0,0 +1,28 @@
+/*
+    This file was generated and must not be modified.
+    Any changes made in this file will be overriden.
+    If you want to make changes, please make them in the source json file
+    and use the python script to generate the Localized.strings
+    as well as the LocalizableKeys.swift file for an iOS project
+    and the strings.xml file for an Anroid project.
+ */
+/**
+ * This enum provides convenient access to the keys which are used to access the localized strings.
+ * It extents the class LocalizableKeys, which provides a base class for all generated enums.
+ *
+ * Just add this class to your project:
+ * ```
+ * class LocalizableKeys {}
+ * ```
+ *
+ * Example for usage:
+ * ```
+ * let key = LocalizableKeys.some_value_of_this_enum.rawValue
+ * let value = Localizer.string(forKey: key)
+ * ```
+ */
+ extension LocalizableKeys {
+    enum FileName: String {
+        case Key1 = "Key1"
+    }
+}
