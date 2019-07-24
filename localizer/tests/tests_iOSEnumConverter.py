3ec1ebc934bd72fb2b598be438cc44239cc43417
David Piper <david.piper@udo.edu>
2019-07-24 18:55:14 +0300

    Added test for iOSEnumConverter. Removed some unneccessary spaces in localizer/templates/template_common_generated_warning.txt which leeded to errors in test case. Added this change in testdata for iOSConverter and AndroidConverter tests.

---
+++ localizer/tests/tests_iOSEnumConverter.py
@@ -0,0 +1,46 @@
+import unittest
+
+from localizer.lib import FileHelper
+from localizer.lib import JsonHelper
+
+from localizer.model.IntermediateEntry import IntermediateEntry
+from localizer.model.IntermediateLanguage import IntermediateLanguage
+from localizer.model.IntermediateLocalization import IntermediateLocalization
+from localizer.model.LocalizationFile import LocalizationFile
+
+from localizer.converter.iOSEnumConverter import iOSEnumConverter
+
+class TestiOSEnumConverter(unittest.TestCase):
+
+    # common subject under test for all test cases
+    sut = iOSEnumConverter()
+
+    #--------------------------------------------------
+    # Testcases for main functionality
+    #--------------------------------------------------
+
+    # Method toIntermediate not tested, because this converter provides no import functionality.
+
+    def test_fromIntermediate(self):
+        expectedFilepath = "FileNameLocalizableKeys.swift"
+        expectedContent = FileHelper.readFile("localizer/tests/testdata/FileNameLocalizableKeys.swift")
+        expectation = LocalizationFile(expectedFilepath, expectedContent)
+        intermediate = self._createExampleIntermediateLocalization()
+        result = self.sut.fromIntermediate(intermediate)[0]
+        self.assertEqual(expectation.filepath, result.filepath)
+
+    #--------------------------------------------------
+    # Testcases for helper methods
+    #--------------------------------------------------
+
+    #--------------------------------------------------
+    # Private test helper
+    #--------------------------------------------------
+
+    def _createExampleIntermediateLocalization(self):
+        entry = IntermediateEntry("Key1", "Value1")
+        language = IntermediateLanguage("ExampleLanguage", [entry])
+        return IntermediateLocalization("FileName", [language])
+
+if __name__ == '__main__':
+    unittest.main()
