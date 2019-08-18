e43b751ea059d871f515a0171450d20c0cfe98a3
David Piper <david.piper@udo.edu>
2019-08-18 13:06:01 +0200

    updated setup (added entry_points and long_description

--- setup.py
+++ setup.py
@@ -2,20 +2,23 @@ from distutils.core import setup
 setup(
   name = "Logen",
   packages = ["Logen"],
+  entry_points = {
+      "console_scripts": ["logen = Logen.main"]
+  },
   version = "0.1.0",
   license = "MIT",
   description = "Generates and converts between various localization formats.",
+  long_description = "Generates and converts between various localization formats.",
   author = "David Piper",
   author_email = "david@dpiper.de",
   url = "https://github.com/DavidPiper94",     # Provide either the link to your github or to your website
-  download_url = "https://github.com/DavidPiper94/Logen/archive/v_0.1.0.tar.gz",
+  download_url = "https://github.com/DavidPiper94/Logen/archive/v_0.1.1.tar.gz",
   keywords = [
       "localization",
       "converter",
       "ios",
       "android",
     ],
-  install_requires = [],
   classifiers = [
     "Development Status :: 3 - Alpha",          # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     "Intended Audience :: Developers",          # Define that your audience are developers
