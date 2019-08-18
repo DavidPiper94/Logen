4b0d67b235c2c22bce7911254510cdad7e678b76
David Piper <david.piper@udo.edu>
2019-08-18 12:37:36 +0200

    Added setup and license files. Updated readme.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = "Logen",
+  packages = ["Logen"],
+  version = "0.1.0",
+  license = "MIT",
+  description = "Generates and converts between various localization formats.",
+  author = "David Piper",
+  author_email = "david@dpiper.de",
+  url = "https://github.com/DavidPiper94",     # Provide either the link to your github or to your website
+  download_url = "https://github.com/DavidPiper94/Logen/archive/v_0.1.0.tar.gz",
+  keywords = [
+      "localization",
+      "converter",
+      "ios",
+      "android",
+    ],
+  install_requires = [],
+  classifiers = [
+    "Development Status :: 3 - Alpha",          # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    "Intended Audience :: Developers",          # Define that your audience are developers
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",      # Specify which pyhton versions that you want to support
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+  ],
+)
