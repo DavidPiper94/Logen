1e63587062be83210ebdf7a304b0208071d66d02
David Piper <david.piper@udo.edu>
2019-08-18 13:52:24 +0200

    Using find_packages in setup.py to find all subpackages automaticaly.

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ import setuptools
 
 setup(
   name = "Logen",
-  packages = ["Logen"],
+  packages = find_packages(),                   # finds all packages and subpackages automatically
   entry_points = {
       "console_scripts": ["logen = Logen:main"]
   },
-  version = "0.1.3",
+  version = "0.1.4",
   license = "MIT",
   description = "Generates and converts between various localization formats.",
   long_description = "Generates and converts between various localization formats.",
   author = "David Piper",
   author_email = "david@dpiper.de",
   url = "https://github.com/DavidPiper94",     # Provide either the link to your github or to your website
-  download_url = "https://github.com/DavidPiper94/Logen/archive/v_0.1.3.tar.gz",
+  download_url = "https://github.com/DavidPiper94/Logen/archive/v_0.1.4.tar.gz",
   keywords = [
       "localization",
       "converter",
