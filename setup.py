3d62e567a8e3963bb205d13a0e77b0ff2833f70a
David Piper <david.piper@udo.edu>
2019-08-18 17:28:09 +0200

    Updated version to 1.0.0

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
   entry_points = {
       "console_scripts": ["logen = Logen.main:main"]
   },
-  version = "0.1.6",
+  version = "1.0.0",
   license = "MIT",
   description = "Generates and converts between various localization formats.",
   long_description = "Generates and converts between various localization formats.",
   author = "David Piper",
   author_email = "david@dpiper.de",
   url = "https://github.com/DavidPiper94",     # Provide either the link to your github or to your website
-  download_url = "https://github.com/DavidPiper94/Logen/archive/v_0.1.6.tar.gz",
+  download_url = "https://github.com/DavidPiper94/Logen/archive/v_1.0.0.tar.gz",
   keywords = [
       "localization",
       "converter",
@@ -23,7 +23,7 @@ setup(
       "android",
     ],
   classifiers = [
-    "Development Status :: 3 - Alpha",          # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    "Development Status :: 4 - Beta",          # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     "Intended Audience :: Developers",          # Define that your audience are developers
     "Topic :: Software Development :: Build Tools",
     "License :: OSI Approved :: MIT License",
